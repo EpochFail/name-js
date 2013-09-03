6cd659bae5d3156bf3f895614ac8efb052bd505e
Carter Bancroft <alt234@gmail.com>
2013-09-03 00:46:43 -0800

    Get some really rudimentary front end functionality for voting going. Vote count can currently get out of sync with db if you vote really fast.

--- js/doit.js
+++ js/doit.js
@@ -1,18 +1,55 @@
-var button = document.getElementById("button");
+var name_button = document.getElementById("button");
+var curr_word = "";
+var curr_rating = 0;
 
-button.addEventListener("click", function() {
+name_button.addEventListener("click", function() {
   var name = document.getElementById("name");
-  
+  var rating = document.getElementById("rating");
+
   var xhr = new XMLHttpRequest();
   
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
       obj = JSON.parse(xhr.responseText);
-      name.innerHTML = obj.word + ".js";
+      
+      curr_word = obj.word;
+      curr_rating = obj.rating;
+      name.innerHTML = curr_word + ".js";
+      rating.innerHTML = curr_rating;;
     }
   };
 
-  xhr.open('GET', 'https://tranquil-lowlands-2993.herokuapp.com/api/word', true);
+  xhr.open('GET', 'http://localhost:5555/api/word', true);
+  xhr.send(null);
+}, false);
+
+var up_button = document.getElementById("up");
+var down_button = document.getElementById("down");
+
+up_button.addEventListener("click", function() {
+  if (curr_word === "") return;
+ 
+  curr_rating++;
+  rating.innerHTML = curr_rating;
+
+  var xhr = new XMLHttpRequest();
+  var url = "http://localhost:5555/api/vote/" + curr_word + "/up";
+  
+  xhr.open('GET', url, true);
+  xhr.send(null);
+  
+}, false);
+
+down_button.addEventListener("click", function() {
+  if (curr_word === "") return;
+  
+  curr_rating--;
+  rating.innerHTML = curr_rating;
+  
+  var xhr = new XMLHttpRequest();
+  var url = "http://localhost:5555/api/vote/" + curr_word + "/down";
+  
+  xhr.open('GET', url, true);
   xhr.send(null);
 }, false);
 
