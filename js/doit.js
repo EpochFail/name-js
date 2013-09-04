c52a10e038978aaaa7761bf0be05de6838d13d38
Carter Bancroft <alt234@gmail.com>
2013-09-04 00:53:50 -0800

    Add voting hotkeys and limit users to one vote total.

--- js/doit.js
+++ js/doit.js
@@ -1,46 +1,77 @@
 var root_url = "http://localhost:5555";
 
 var curr_word = "";
-var curr_rating = 0;
+var starting_rating = 0;
+var session_rating = 0;
 
 var rating_div;
 var name_div;
 
-$(function() {
-  $("#button").click(displayJson);
-	$(document).keydown(function(e) { if (e.keyCode == 32) displayJson(); });
+var upvote_button;
+var downvote_button;
 
-  $("#up").click({direction: "up"}, vote);
-  $("#down").click({direction: "down"}, vote);
+$(function() {
+  $("#button").click(generateName);
+	$(document).keyup(function(e) { if (e.keyCode == 32 || e.keyCode == 13) generateName(); });
+
+	upvote_button = $("#up");
+  downvote_button = $("#down");
+
+  upvote_button.click({direction: "up"}, vote);
+  downvote_button.click({direction: "down"}, vote);
+  $(document).keyup(function(e) { if (e.keyCode == 38) upvote_button.click(); });
+  $(document).keyup(function(e) { if (e.keyCode == 40) downvote_button.click(); });
 
   rating_div = $("#rating");
   name_div = $("#name");
 });
 
-function displayJson() {
+function generateName() {
   $.getJSON(root_url + "/api/word", function(data) {
     curr_word = data.word;
-    curr_rating = data.rating;
+    starting_rating = data.rating;
+    session_rating = starting_rating;
     
     name_div.html(data.word + ".js");
-    rating_div.html(curr_rating);
+    rating_div.html(starting_rating);
     
     votes.style.visibility = "visible";
-  });
+		resetVoteImages();	
+	});
 }
+
+var user_voted = false;
+var user_voted_direction;
 
 function vote(event) {
   if (curr_word === "") return;
 
   var direction = event.data.direction;
   
-  if (direction === "up") curr_rating++;
-  else if (direction === "down") curr_rating--;
+  if (direction === "up" && session_rating < starting_rating + 1) {
+    session_rating++;
+		upvote_button.attr("src", "img/upvote-pressed.png");
+    updateVote(direction);
+  }
+  else if (direction === "down" && session_rating > starting_rating - 1) {
+    session_rating--;
+		downvote_button.attr("src", "img/downvote-pressed.png");
+    updateVote(direction);
+  }
 
-  rating_div.html(curr_rating);
+	if (session_rating == starting_rating) resetVoteImages();
+}
+
+function updateVote(direction) {
+  rating_div.html(session_rating);
 
   var _url = root_url + "/api/vote/" + curr_word + "/" + direction;
   $.ajax({
     url: _url
   });
+}
+
+function resetVoteImages() {
+	upvote_button.attr("src", "img/upvote.png");
+	downvote_button.attr("src", "img/downvote.png");
 }
