a7e352ece99a1ad83ffb96e5eb2b05272406368e
Carter Bancroft <alt234@gmail.com>
2013-09-04 00:12:36 -0800

    Switch to using jQuery and add spacebar press event.

--- js/doit.js
+++ js/doit.js
@@ -1,18 +1,11 @@
-var button = document.getElementById("button");
+$(function() {
+  $("#button").click(displayJson);
+	$(document).keydown(function(e) { if (e.keyCode == 32) displayJson(); });
 
-button.addEventListener("click", function() {
-  var name = document.getElementById("name");
-  
-  var xhr = new XMLHttpRequest();
-  
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      obj = JSON.parse(xhr.responseText);
-      name.innerHTML = obj.word + ".js";
-    }
-  };
-
-  xhr.open('GET', 'https://tranquil-lowlands-2993.herokuapp.com/api/word', true);
-  xhr.send(null);
-}, false);
+	function displayJson() {
+    $.getJSON("https://tranquil-lowlands-2993.herokuapp.com/api/word", function(data) {
+      $("#name").html(data.word + ".js");
+    });
+	}
+});
 
